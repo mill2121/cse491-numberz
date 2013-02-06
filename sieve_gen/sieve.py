5d77cf726f3327f9c5ceabaa8ec62d156a4f413f
Eric Miller <eric@TheColonel.(none)>
2013-02-05 19:54:30 -0500

    Fixed test files

--- sieve_gen/sieve.py
+++ sieve_gen/sieve.py
@@ -1,8 +1,6 @@
 # an implementation of Eratosthenes' Sieve:
 #   http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 
-_primeslist = [2]
-
 # Checks whether a number is prime or not
 def _is_prime(primes, n):
     for i in primes:
@@ -12,6 +10,8 @@ def _is_prime(primes, n):
 
 # Loops through numbers and jumps back to the test files once one is found
 def sieve():
+    _primeslist = [2]
+    
     start = _primeslist[-1] + 1
     while 1:
         if _is_prime(_primeslist, start):
--- sieve_iter/test1.py
+++ sieve_iter/test1.py
@@ -1,5 +1,33 @@
 import sieve
 
-for n, i in zip(range(10), sieve.sieve()):
-    print i
+def test1():
+    sve = sieve.sieve()
+    i = iter(sve)
+    nxt = i.next()
+    print nxt
+    assert nxt == 3
 
+def test2():
+    sve = sieve.sieve()
+    i = iter(sve)
+    nxt = i.next()
+    print nxt
+    nxt = i.next()
+    print nxt
+    assert nxt == 5
+
+def test3():
+    sve = sieve.sieve()
+    i = iter(sve)
+    nxt = i.next()
+    print nxt
+    nxt = i.next()
+    print nxt
+    nxt = i.next()
+    print nxt
+    assert nxt == 7
+
+test1()
+test2()
+test3()
+    
